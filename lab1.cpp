0705547bbe8c8d5a3088dddb8b933260db7c16d7
mztejerobcc <30049055@cc.peralta.edu>
2026-01-26 19:51:03 +0000

    update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,10 +1,14 @@
 /*
+
 Write a short C++ program that does the following:
 
 Declares an integer variable named points and a string variable named name.
 Assigns a number and your name to these variables.
 Prints a message to the console that says "Hello, [name]! The computer has seen fit to assign you [points] points.”
 You can copy-paste your program into the assignment or upload a file.
+
+Please create a new repository in GitHub with your code and turn in the link to the repository.
+
 */
 
 #include <iostream>
@@ -18,8 +22,10 @@ int main() {
 }
 
 /*
+
 Compile and Execute
 
 g++ -std=c++11 -o lab1 lab1.cpp
 ./lab1
+
 */
